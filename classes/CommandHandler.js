85e4979701dc9d5589519fcd4f647066e915ac0a
graylyfire-netizen <graylyfire@gmail.com>
2026-04-14 12:50:15 +0530

    CommandHandler.js

--- classes/CommandHandler.js
+++ classes/CommandHandler.js
@@ -1,9 +1,11 @@
+"use strict";
+
 const ms = require("ms");
 
 const { userModel, createUserDefaults } = require("../common/Database.js");
 
 const Embed = require("./Embed.js");
-const config = require("../ext/config.json");
+const config = require("../config"); // ✅ FIXED (no more config.json)
 
 class CommandHandler {
   constructor(member, guild, command, cooldowns, interaction, args) {
@@ -13,14 +15,19 @@ class CommandHandler {
     this.interaction = interaction;
     this.cooldowns = cooldowns;
     this.args = args;
-    this.whole_command = this.args.sub_command ? `${this.command.name} ${this.args.sub_command}` : this.command.name;
+    this.whole_command = this.args.sub_command
+      ? `${this.command.name} ${this.args.sub_command}`
+      : this.command.name;
+
     this.embed = new Embed();
     this.embed.thumbnail.url = "";
   }
 
   async handle() {
     if (!this.command.dontUseDB) {
-      this.dbUser = await userModel.findOne({ id: this.member.id }) ?? createUserDefaults({ id: this.member.id });
+      this.dbUser =
+        (await userModel.findOne({ id: this.member.id })) ??
+        createUserDefaults({ id: this.member.id });
 
       this.checkPremiumStatus(this.dbUser);
     } else {
@@ -32,7 +39,9 @@ class CommandHandler {
     let cooldownResult = null;
 
     if (this.command.cooldown) {
-      cooldownResult = this.command.dontUseDB ? this.handleCooldown() : this.handleCooldown(this.dbUser);
+      cooldownResult = this.command.dontUseDB
+        ? this.handleCooldown()
+        : this.handleCooldown(this.dbUser);
 
       if (cooldownResult) return "onCooldown";
     }
@@ -52,9 +61,9 @@ class CommandHandler {
       (this.command.dmsOnly && this.guild)
     ) {
       const reason =
-        !config.guildsWhitelist.includes(String(this.guild?.id)) && this.guild ?
-          "You're not in a whitelisted guild." :
-          "This is only usable in DMs.";
+        !config.guildsWhitelist.includes(String(this.guild?.id)) && this.guild
+          ? "You're not in a whitelisted guild."
+          : "This is only usable in DMs.";
 
       this.embed.description = reason;
       this.interaction.createFollowup({ embed: this.embed });
@@ -62,16 +71,22 @@ class CommandHandler {
     }
 
     if (
-      !this.command.dontUseDB && user.blacklisted ||
+      (!this.command.dontUseDB && user.blacklisted) ||
       (this.command.staffOnly && !user.staff) ||
       (this.command.requireLink && !user.didLink) ||
       (this.command.premium && !user.premium)
     ) {
-      const reason = user.blacklisted ? "You're blacklisted."
-        : this.command.staffOnly && !user.staff ? "You don't have access to use this command."
-          : this.command.requireLink && !user.didLink ? "This requires you to link."
-            : this.command.premium && !user.premium ? "This command requires premium."
-              : this.command.notUpdated ? "This command is not updated to the current version." : "";
+      const reason = user.blacklisted
+        ? "You're blacklisted."
+        : this.command.staffOnly && !user.staff
+        ? "You don't have access to use this command."
+        : this.command.requireLink && !user.didLink
+        ? "This requires you to link."
+        : this.command.premium && !user.premium
+        ? "This command requires premium."
+        : this.command.notUpdated
+        ? "This command is not updated to the current version."
+        : "";
 
       this.embed.description = reason;
       this.interaction.createFollowup({ embed: this.embed });
@@ -82,7 +97,7 @@ class CommandHandler {
   }
 
   handleCooldown(user) {
-    // No staff cooldown :yawning_face:
+    // No staff cooldown
     if (typeof user === "object" && user?.staff) return false;
 
     const now = Date.now();
@@ -93,15 +108,26 @@ class CommandHandler {
 
     let cooldownDuration = 0;
     if (typeof this.command.cooldown === "object") {
-      cooldownDuration = this.command.cooldown[sub_command] ?? this.command.cooldown.global ?? 0;
+      cooldownDuration =
+        this.command.cooldown[sub_command] ??
+        this.command.cooldown.global ??
+        0;
     } else {
       cooldownDuration = this.command.cooldown ?? 0;
     }
 
-    if (Object.keys(cooldownMap).length !== 0 && cooldownMap[this.whole_command]) {
-      rateLimitEnd = ms(cooldownMap[this.whole_command] - now, { long: true });
+    if (
+      Object.keys(cooldownMap).length !== 0 &&
+      cooldownMap[this.whole_command]
+    ) {
+      rateLimitEnd = ms(cooldownMap[this.whole_command] - now, {
+        long: true
+      });
 
-      if (!rateLimitEnd.includes("ms") && cooldownMap[this.whole_command] > now) {
+      if (
+        !rateLimitEnd.includes("ms") &&
+        cooldownMap[this.whole_command] > now
+      ) {
         this.embed.description = `This is on cooldown for **${rateLimitEnd}**.`;
         this.interaction.createFollowup({ embed: this.embed });
         return true;
